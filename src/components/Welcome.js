9d0dc592f036c8ae1d75b516d25089fe312b8630
theayan44 <88241719+theayan44@users.noreply.github.com>
2022-02-05 01:20:34 +0530

    Change

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -4,7 +4,7 @@ import React from "react";
 const Welcome = (props) => {
   return (
     <>
-      <h1>Hey !{this.props.name}</h1>
+      <h1>Hey !{props.name}</h1>
       <h2>Welcome to Newton School</h2>
     </>
   );

39132705eb2db35298a3ab9124edf352d159f4d6
theayan44 <88241719+theayan44@users.noreply.github.com>
2022-02-05 01:14:01 +0530

    change

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,1 +1,13 @@
 // write code for Welcome component here
+import React from "react";
+
+const Welcome = (props) => {
+  return (
+    <>
+      <h1>Hey !{this.props.name}</h1>
+      <h2>Welcome to Newton School</h2>
+    </>
+  );
+};
+
+export default Welcome;
